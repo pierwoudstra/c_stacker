17c79b1dc4bcabe0c3433a801e4c1d62938f73da
pier woudstra <pier.johannes@gmail.com>
2026-04-01 01:41:19 +0200

    added pop

--- interpreter.h
+++ interpreter.h
@@ -1,7 +1,9 @@
 
 void interpret_cmd(f_stack* s, string_view cmd) {
-    
-    // convert to float and push to stack
-    push_f(s, strtof(cmd.data, NULL)); 
-
+    if (sv_compare(cmd, sv("pop"))) {
+        pop_f(s);
+    } else {
+        // convert to float and push to stack
+        push_f(s, strtof(cmd.data, NULL));
+    }
 }
